a62eb294caa4346d9ab98306c6126d92b60c03e9
m1ras01 <221930@astanait.edu.kz>
2023-04-19 01:21:00 +0600

    s

---
+++ src/MyArrayList.java
@@ -0,0 +1,14 @@
+public class MyArrayList<E>{
+     private Object[] arr;
+     private int size=5;
+     MyArrayList(){
+         arr = new Object[5];
+
+     }
+     public void add(E element){
+         if(size == arr.length){
+             Object[] newArr = new Object[arr.length*2];
+
+         }
+     }
+}
