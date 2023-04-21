c3b29e1ae46538ab42ab1d4e9a3261c0244485a1
m1ras01 <221930@astanait.edu.kz>
2023-04-21 20:02:18 +0600

    linkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,12 @@
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    private class Node {
+        private E element;
+        private Node next;
+        private Node prev;
+        public Node(E element){
+           this.element = element;
+           this.next = null;
+           this.prev = null;
+        }
+    }
 }
